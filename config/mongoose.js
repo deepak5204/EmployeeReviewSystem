5c88fba61aad9d95b789fa674445b7d70cf5895d
Deepak <deepakkumar750206@gmail.com>
2023-03-22 14:13:01 +0530

    Install mongoose and successfully connect database

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 
 try {
-     mongoose.connect('mongodb://127.0.0.1:27017/test');
+     mongoose.connect('mongodb://127.0.0.1:27017/EmployeeReviewSystem');
         console.log('DB connected successfully!');   
   } catch (error) {
         console.log(error);
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+// const mongoose = require('./config/mongoose');
 const app = express(); 
 const port = 8000;
 
 
 app.listen(port, '127.0.0.1', ()=>{
     console.log(`server is running on ${port}`);
+    
 });
 
